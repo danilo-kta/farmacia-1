e68d57f9e14b54795b15212f36a2c5014ed3482b
danilo-kta <65986482+danilo-kta@users.noreply.github.com>
2022-11-26 22:42:35 -0300

    Trabalho Luis
    Trabalho Luis, desenvolvido Juntamente com Lucas Schelb
    
    Co-Authored-By: Lucasschelb <65633364+Lucasschelb@users.noreply.github.com>

---
+++ src/database/data.tsx
@@ -0,0 +1,26 @@
+const produto_1 = {
+    codigo: 1,
+    nome: "Dipirona",
+    descricao: "Para dor de cabeça devido a programação",
+    valor: 100.00,
+    categoria: {
+      codigo: 1,
+      descricao: "Medicamento"
+    }
+  };
+  
+  const produto_2 = {
+    codigo: 2,
+    nome: "Alivium",
+    descricao: "Para dor de cabeça devido a programação",
+    valor: 150.00,
+    categoria: {
+      codigo: 1,
+      descricao: "Medicamento"
+    }
+  };
+  
+  export const produtos = [
+    produto_1,
+    produto_2
+  ];
---
+++ src/entidades/produto.tsx
@@ -0,0 +1,8 @@
+import { Categoria } from './categoria';
+
+export interface Produto{
+    codigo: number;
+    nome: string;
+    descricao: string;
+    valor: number;
+}
